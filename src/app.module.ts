ba0f448c37c7019c8cbb91208225773c557e6bea
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-17 12:44:42 +0330

    Feat: Add OpenAI Controller,Service,Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { OpenaiModule } from './openai/openai.module';
 
 @Module({
-  imports: [ConfigModule.forRoot({ isGlobal: true })],
+  imports: [ConfigModule.forRoot({ isGlobal: true }), OpenaiModule],
   controllers: [],
   providers: [],
 })
