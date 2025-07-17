2bf257f4a0a876cd82c680031bdb4c6714ee4813
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-17 23:25:30 +0330

    Feat: Add Post Method chatCompletion & Use Interface For Body

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,4 +1,8 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { ChatCompletionMessageDto } from './dto/create-chat-completion.request';
 
 @Controller('openai')
-export class OpenaiController {}
+export class OpenaiController {
+  @Post('chatCompletion')
+  async CreateChatCompletion(@Body() body: ChatCompletionMessageDto) {}
+}
