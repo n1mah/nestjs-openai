61da282bcc38617549675d6ab96af310685aa20e
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-17 23:32:00 +0330

    Feat: Implement Validation For create-chat-completion

--- src/openai/dto/create-chat-completion.request.ts
+++ src/openai/dto/create-chat-completion.request.ts
@@ -1,8 +1,19 @@
+import { IsArray, IsNotEmpty, IsString, ValidateNested } from 'class-validator';
+import { Type } from 'class-transformer';
+
 export class ChatCompletionMessageDto {
+  @IsString()
+  @IsNotEmpty()
   role: string;
+
+  @IsString()
+  @IsNotEmpty()
   content: string;
 }
 
 export class CreateChatCompletion {
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ChatCompletionMessageDto)
   message: ChatCompletionMessageDto[];
 }
