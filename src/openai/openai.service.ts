cf05fe08e350e58f2cf1d5e8aefa3bb44ff47e82
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-18 11:03:54 +0330

    Feat: Implement OpenAi Service

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { OpenAI } from 'openai';
+import { ChatCompletionMessageDto } from './dto/create-chat-completion.request';
+import { ChatCompletionMessageParam } from 'openai/resources';
 
 @Injectable()
-export class OpenaiService {}
+export class OpenaiService {
+  constructor(private readonly openAI: OpenAI) {}
+
+  async createChatCompletion(message: ChatCompletionMessageDto[]){
+    return this.openAI.chat.completions.create({
+      messages: message as ChatCompletionMessageParam[],
+      model: 'openai/gpt-4o',
+      max_tokens: 1000,
+    });
+  }
+}
