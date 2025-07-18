9d75154c68c8f0fd210a52e88db26aa9b41a3d89
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-18 14:51:56 +0330

    Feat: Implement OpenService With INSTRUCTION & Content & Role

--- src/openai/openai.service.ts
+++ src/openai/openai.service.ts
@@ -3,14 +3,28 @@ import { OpenAI } from 'openai';
 import { ChatCompletionMessageDto } from './dto/create-chat-completion.request';
 import { ChatCompletionMessageParam } from 'openai/resources';
 
+const MODEL = 'openai/gpt-4o';
+const INSTRUCTION = `
+Extract all numbers from the user's message and return them as a comma-separated list.
+Do not include any explanation or extra text.
+`;
+
 @Injectable()
 export class OpenaiService {
   constructor(private readonly openAI: OpenAI) {}
 
-  async createChatCompletion(message: ChatCompletionMessageDto[]){
+  async createChatCompletion(userMessages: ChatCompletionMessageDto[]){
+    const messages: ChatCompletionMessageParam[] = [
+      { role: 'system', content: INSTRUCTION },
+      ...userMessages.map(({ role, content }: ChatCompletionMessageDto) => ({
+        role,
+        content,
+      })),
+    ];
+
     return this.openAI.chat.completions.create({
-      messages: message as ChatCompletionMessageParam[],
-      model: 'openai/gpt-4o',
+      messages,
+      model: MODEL,
       max_tokens: 1000,
     });
   }
