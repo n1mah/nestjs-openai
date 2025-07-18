7fbaf7c21fd40d9617b9ec2e3b093aa3a33faef3
Nima Heidari <nima.heidari.n@gmail.com>
2025-07-18 11:04:49 +0330

    Feat: Implement OpenAi Controller For chatCompletion Method

--- src/openai/openai.controller.ts
+++ src/openai/openai.controller.ts
@@ -1,8 +1,13 @@
 import { Body, Controller, Post } from '@nestjs/common';
-import { ChatCompletionMessageDto } from './dto/create-chat-completion.request';
+import { CreateChatCompletion } from './dto/create-chat-completion.request';
+import { OpenaiService } from './openai.service';
 
 @Controller('openai')
 export class OpenaiController {
+  constructor(private readonly openaiService: OpenaiService) {}
+
   @Post('chatCompletion')
-  async CreateChatCompletion(@Body() body: ChatCompletionMessageDto) {}
+  async CreateChatCompletion(@Body() body: CreateChatCompletion) {
+    return this.openaiService.createChatCompletion(body.message);
+  }
 }
